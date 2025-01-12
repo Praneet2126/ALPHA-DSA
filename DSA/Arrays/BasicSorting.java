8067a0f9170b0b5c9336d39a63ab4fd227a10757
Praneet <praneetvkhp@gmail.com>
2025-01-12 15:09:21 +0530

    Insertion Sort

--- DSA/Arrays/BasicSorting.java
+++ DSA/Arrays/BasicSorting.java
@@ -39,10 +39,23 @@ public class BasicSorting {
         }
     }
 
+    public static void insertionSort(int arr[]) {
+        for(int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            while (prev>=0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {5,4,3,2,1};
         // bubbleSort(nums);
-        selectionSort(nums);
+        // selectionSort(nums);
+        insertionSort(nums);
         printArr(nums);        
     }    
 }
