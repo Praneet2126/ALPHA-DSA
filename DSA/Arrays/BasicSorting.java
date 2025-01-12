1894e513311156c3869f54dd3f3ddc466a959af4
Praneet <praneetvkhp@gmail.com>
2025-01-12 14:54:57 +0530

    Selection Sort

--- DSA/Arrays/BasicSorting.java
+++ DSA/Arrays/BasicSorting.java
@@ -24,9 +24,25 @@ public class BasicSorting {
         }
     }
 
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {5,4,3,2,1};
-        bubbleSort(nums);
+        // bubbleSort(nums);
+        selectionSort(nums);
         printArr(nums);        
     }    
 }
