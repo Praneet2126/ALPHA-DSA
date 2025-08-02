09fd7da568f8455016857f5302f03bc3499f1180
Praneet <praneetvkhp@gmail.com>
2025-08-03 00:37:34 +0530

    Cycle Detection

---
+++ PatternWise/Graphs/CycleDetection/CycleDetect.java
@@ -0,0 +1,46 @@
+package PatternWise.Graphs.CycleDetection;
+
+import java.util.*;
+
+public class CycleDetect {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean[] vis = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(!vis[i]) {
+                if(detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int par) {
+        vis[curr] = true;
+        for(int i=0; i<graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]) {
+                if(detectCycleUtil(graph, vis, e.dest, curr)) {
+                    return true;
+                }
+            } else if(vis[e.dest] && e.dest != par) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
